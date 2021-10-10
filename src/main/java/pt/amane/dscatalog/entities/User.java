e82ad85dd50b30baadc4e76748feb39e836a9891
Ansumane Mané <manoansu@gmail.com>
2021-10-10 14:24:05 +0100

    ajust token error

--- src/main/java/pt/amane/dscatalog/entities/User.java
+++ src/main/java/pt/amane/dscatalog/entities/User.java
@@ -2,7 +2,6 @@ package pt.amane.dscatalog.entities;
 
 import java.io.Serializable;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -131,8 +130,7 @@ public class User implements UserDetails, Serializable {
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
